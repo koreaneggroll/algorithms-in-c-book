3740d07cba7e9664cb256f6bd1b90f1dbfc68c9d
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-03-09 16:16:09 +0200

    euclids algorithm

---
+++ C/euclids_alg.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+/*
+    Euclid's method is based on the fact that if 'u' is greater than 'v' then the greatest
+common divisor of 'u' and 'v' is the same as the greatest common divisor of 'v' and u - v. This observation
+leads to the following implementation in C
+*/
+
+int gcd(int u, int v){
+    int t;
+
+    while(u > 0){
+
+        if(u < v){
+
+            t = u;
+            u = v;
+            v = t;
+
+        }
+
+        u = u - v;
+    }
+    
+    return v;
+}
+
+int main(void){
+    int x, y;
+
+    while(scanf("%d %d", &x, &y) != EOF){
+
+        if(x > 0 && y > 0){
+            printf("%d %d %d\n", x, y, gcd(x, y));
+        }
+
+    }
+
+}
